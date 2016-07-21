8d68ac6f40b41138081df036b23b9dcdfd99b535
johnsensjr <334269511@qq.com>
2016-07-21 15:58:03 +0800

    新建博客基本布局

--- myblog/routes/index.js
+++ myblog/routes/index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: '' ,layout:'template'});
 });
+/* POST home page. */
+router.post('/post', function(req, res) {
 
+});
+/* REG get. */
+router.get('/reg', function(req, res) {
+
+});
+/* REG post. */
+router.post('/reg', function(req, res) {
+
+});
+/* login get. */
+router.get('/login', function(req, res) {
+
+});
+/* login post. */
+router.post('/login', function(req, res) {
+
+});
+/* logout get. */
+router.get('/logout', function(req, res) {
+
+});
 module.exports = router;
